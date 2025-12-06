d41e9f94077710540bf1550f1f898a7381f0157e
Ehsan Ansari Basir <ehsan.ansari@instabee.com>
2025-12-06 08:45:25 +0100

    day 4 p1

---
+++ day4/p1/solution.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"adventofcode2025/utils"
+	"fmt"
+	"math"
+)
+
+type Role struct {
+	i, j      int
+	neighbors []*Role
+}
+
+func main() {
+	lines := utils.ReadInput("day4/input.txt")
+	places := get_places(lines)
+	roles := get_roles(places)
+
+	form_relations(places, roles)
+
+	fmt.Printf("\nAnswer: %d\n", get_answer(places, roles))
+
+}
+
+func get_places(lines []string) [][]rune {
+	places := [][]rune{}
+	for _, line := range lines {
+
+		new_row := []rune{}
+		for _, c := range line {
+			new_row = append(new_row, c)
+		}
+		places = append(places, new_row)
+	}
+	return places
+}
+
+func get_roles(places [][]rune) map[string]*Role {
+	roles := make(map[string]*Role)
+	for i := 0; i < len(places); i++ {
+		for j := 0; j < len(places[i]); j++ {
+			if places[i][j] == '@' {
+				roles[fmt.Sprintf("%d-%d", i, j)] = &Role{
+					i:         i,
+					j:         j,
+					neighbors: []*Role{},
+				}
+			}
+		}
+	}
+	return roles
+}
+
+func form_relations(places [][]rune, roles map[string]*Role) {
+	// Second pass: establish neighbor relationships
+	for key, role := range roles {
+		for i := int(math.Max(0, float64(role.i-1))); i <= int(math.Min(float64(len(places)-1), float64(role.i+1))); i++ {
+			for j := int(math.Max(0, float64(role.j-1))); j <= int(math.Min(float64(len(places[0])-1), float64(role.j+1))); j++ {
+				if i == role.i && j == role.j {
+					continue
+				}
+				if neighbor, ok := roles[fmt.Sprintf("%d-%d", i, j)]; ok {
+					role.neighbors = append(role.neighbors, neighbor)
+				}
+			}
+		}
+		_ = key
+	}
+}
+
+func get_answer(places [][]rune, roles map[string]*Role) int {
+	rows := len(places)
+	cols := len(places[0])
+
+	answer := 0
+
+	i := 0
+
+	for i < rows {
+		j := 0
+		for j < cols {
+			if role, ok := roles[fmt.Sprintf("%d-%d", i, j)]; ok {
+				if len(role.neighbors) < 4 {
+					fmt.Printf("x")
+					answer += 1
+				} else {
+					fmt.Printf("@")
+				}
+
+			} else {
+				fmt.Printf(".")
+			}
+			j += 1
+		}
+		fmt.Printf("\n")
+		i += 1
+	}
+
+	return answer
+
+}
